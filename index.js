2675f9e854a6f2057d8a5c21a0558d5dc4f508fe
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2024-08-25 11:53:28 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const functions = require("firebase-functions")
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
